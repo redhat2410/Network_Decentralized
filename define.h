4ff16aa6c37c63ce5a960596a633ef0d350dba33
redhat2410 <ducvuminhspk@gmail.com>
2021-08-22 08:05:23 +0700

    upload by ducvu

--- define.h
+++ define.h
@@ -8,4 +8,15 @@ typedef unsigned int    word;
 
 #define DEBUG
 
+//detect platform os
+#if defined(__linux__)
+    #define PLATFORM_NAME   "linux"
+#elif defined(_WIN32)
+    #define PLATFORM_NAME   "windows"
+#elif defined(_WIN64)
+    #define PLATFORM_NAME   "windows"
+#elif defined(__CYGWIN__) && !defined(_WIN32)
+    #define PLATFORM_NAME   "windows"
+#endif
+
 #endif
